d54a56232fc448302649aaf9834edadb781d5eee
Sara Garcia <saragca81@gmail.com>
2023-10-01 13:33:54 -0700

    added seeds file

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (department_name, department_ids)
+VALUES ();
+/*exp ("food and bev",1)*/
+INSERT INTO  roles (job_title, role_id, department, salary)
+VALUES ();
+/*exp ("supervisor",1,"food and bev", 32000)*/
+
+INSERT INTO employee (employee_id, first_name, last_name, role, manager,)
+VALUES();
+/*exp(1,"Jane", "Smith", "supervisor", "Mary","shelley")*/
---
+++ server.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'test'
+});
