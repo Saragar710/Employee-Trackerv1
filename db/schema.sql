bf6026a8a7ba108de9d557bc11d3ee84f185b3c7
Sara Garcia <saragca81@gmail.com>
2023-10-20 19:07:31 -0700

    added id to primary key

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE  employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY (id),
     name VARCHAR(30) /*TO HOLD DEPT NAME*/
 )
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY (id),
     title VARCHAR(30), /*TO HOLD ROLE TITLE*/
     salary DECIMAL, /*TO HOLD ROLE SALARY*/
     department_id  INT/*TO HOLD REFERENCE TO DEPARTMENT ROLE BELONGS TO */
 )
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY (id),
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     roles_id INT,
